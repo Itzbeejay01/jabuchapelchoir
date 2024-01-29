8503fba0c49d2459db35623a276805a85ee02680
Omolayoayokanmi <97984877+Itzbeejay01@users.noreply.github.com>
2024-01-29 12:52:58 +0100

    Add files via upload

---
+++ api/index.php
@@ -0,0 +1,72 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JABU JCC</title>
+   <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+<link rel="shortcut icon" href="https://arena.mycredentials.ng/wp-content/uploads/2021/04/jabu.jpg" type="image/x-icon">
+</head>
+</head>
+
+<body class="w-full h-full bg-gray-700 py-24 p-10 lg:px-96 lg:py-20">
+    <div class="w-full h-full bg-white rounded py-16 p-8 shadow-md lg:w-full lg:flex lg:flex-col lg:py-20 lg:justify-center lg:items-center">
+        <div class="flex flex-col items-center">
+            <img class="w-24" src="https://arena.mycredentials.ng/wp-content/uploads/2021/04/jabu.jpg" alt="">
+            <h2 class="text-xl text-slate-900 font-bold mb-2 text-center">JOSEPH AYO BABALOLA UNIVERSITY</h2>
+            <h2 class="text-lg text-slate-900 mb-4 text-center">JABU CHAPEL CHOIR</h2>
+        </div>
+        <form id="choirForm" method="post" aria-busy="" class="w-full h-full pb-3 lg:px-14">
+         <?php include 'connection.php';
+         include 'submit.php'; ?>  
+        <div class="mb-4">
+            <input type="text" id="fullname" name="fullname" class="border-2 outline-none rounded-md form-input mt-1 p-2 w-full placeholder:text-gray-800"
+                placeholder="Fullname"  required>
+          </div>
+            <div class="mb-4">
+                <select id="part" name="part" class="text-gray-800 border-2 outline-none rounded-md form-select mt-1 p-2 w-full" required>
+                    <option value="" disabled selected>Select Part</option>
+                    <option value="1">Soprano</option>
+                    <option value="2">Alto</option>
+                    <option value="3">Tenor</option>
+                    <option value="4">Bass</option>
+                </select>
+            </div>
+            <div class="mb-4">
+                <select id="gender" name="gender" class="text-gray-800 border-2 outline-none rounded-md form-select mt-1 p-2 w-full" required>
+                    <option value="" disabled selected>Gender</option>
+                    <option value="1">Male</option>
+                    <option value="2">Female</option>
+                </select>
+            </div>
+            <div class="mb-4">
+                <select id="level" name="level" class="text-gray-800 border-2 outline-none rounded-md form-select mt-1 p-2 w-full" required>
+                    <option value="" disabled selected>Select Level</option>
+                    <option value="1">100</option>
+                    <option value="2">200</option>
+                    <option value="3">300</option>
+                    <option value="4">400</option>
+                    <option value="5">500</option>
+                </select>
+            </div>
+            <div class="mb-4">
+                <input type="text" id="phone" name="phone" class="border-2 outline-none rounded-md form-input mt-1 p-2 w-full placeholder:text-gray-800"
+                    placeholder="Phone Number"  required>
+              </div>
+
+            <button name="login" type="submit" class="w-full bg-blue-500 hover:bg-blue-900 text-white p-2 rounded shadow-lg font-bold text-lg hover:bg-slate-500 active:bg-slate-500">Submit</button>
+        </form>
+    </div>
+    <script>
+        document.getElementById('choirForm').addEventListener('submit', function(evt) {
+          var phone = document.getElementsByName('phone')[0].value;
+          if (phone.length !== 11) {
+            alert('Phone number must be 11 digits long');
+            evt.preventDefault();
+          }
+        });
+      </script>
+</body>
+</html>
---
+++ api/soprano.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Soprano Members</title>
+    <link rel="shortcut icon" href="https://arena.mycredentials.ng/wp-content/uploads/2021/04/jabu.jpg" type="image/x-icon">
+
+
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+        th, td {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+        th {
+            background-color: #00e676;
+            color: rgb(12, 12, 13);
+        }
+        h3{
+            text-align: center;
+            padding: 10px;
+            
+        }
+        body{
+            font-family: "segoe-ui", "open-sans", tahoma, arial;
+			padding: 0;
+			margin: 0;
+        }
+    </style>
+</head>
+<body>
+    <h3>SOPRANO MEMBER LIST</h3> <hr>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Full Name</th>
+            <th>Part</th>
+            <th>Gender</th>
+            <th>Level</th>
+            <th>Phone</th>
+        </tr>
+        <?php
+$servername = 'db4free.net';
+$username = "choirmembers";
+$password = "choirmembers";
+$dbname = "choir_members";
+$dbport = 3306;
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname, $dbport);
+
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        // Query the database to fetch data from the tblsoprano table
+        $sql = "SELECT id, fullname, part, gender, level, phone FROM tblsoprano";
+        $result = $conn->query($sql);
+
+        // Output data in a table-friendly format
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>" . $row["id"] . "</td>";
+                echo "<td>" . $row["fullname"] . "</td>";
+                echo "<td>" . $row["part"] . "</td>";
+                echo "<td>" . $row["gender"] . "</td>";
+                echo "<td>" . $row["level"] . "</td>";
+                echo "<td>" . $row["phone"] . "</td>";
+                echo "</tr>";
+            }
+        } else {
+            echo "<tr><td colspan='6'>0 results</td></tr>";
+        }
+        $conn->close();
+        ?>
+    </table>
+</body>
+</html>
